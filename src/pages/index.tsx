f74c4159337475d2723852092b8d5ed03d0d9592
arthur brito <arthurmedeiros32@gmail.com>
2024-08-22 15:27:14 -0300

    fix: update meta content description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Strateegia survey</title>
         <meta
           name="description"
-          content="Crie pontos de partida com conteúdos de referência"
+          content="Faça pesquisas usando o poder de Strateegia"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
